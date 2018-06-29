7f2b17d840578393c33c045d0254f3ec065f9171
Jonathan Lange <jml@mumak.net>
2018-06-29 20:03:07 +0100

    Separate out request log middleware

--- src/JmlSvc.hs
+++ src/JmlSvc.hs
@@ -12,23 +12,26 @@ import qualified Options.Applicative as Options
 
 import qualified JmlSvc.Logging as Logging
 import qualified JmlSvc.Metrics as Metrics
+import qualified JmlSvc.RequestLog as RequestLog
 import qualified JmlSvc.Warp as Warp
 
 -- | Generic web server configuration.
 data Config = Config
   { loggingConfig :: Logging.Config
   , metricsConfig :: Metrics.Config
+  , requestLogConfig :: RequestLog.AccessLogLevel
   , warpConfig :: Warp.Config
   } deriving (Eq, Show)
 
 -- | Command-line flags for generating 'Config'.
 flags :: Options.Parser Config
-flags = Config <$> Logging.flags <*> Metrics.flags <*> Warp.flags
+flags = Config <$> Logging.flags <*> Metrics.flags <*> RequestLog.flags <*> Warp.flags
 
 -- | Run a web server for 'app'. Blocks until the server is shut down.
 run :: MonadIO io => Text -> Config -> Wai.Application -> io ()
 run appName Config {..} app =
   Logging.run loggingConfig $ do
     prometheus <- Metrics.prometheus appName metricsConfig
-    Warp.run warpConfig (prometheus app)
+    let logging = RequestLog.toMiddleware requestLogConfig
+    Warp.run warpConfig (logging (prometheus app))
     logInfoN $ appName <> " terminated successfully"
--- src/JmlSvc/Warp.hs
+++ src/JmlSvc/Warp.hs
@@ -1,6 +1,5 @@
 module JmlSvc.Warp
   ( Config(..)
-  , AccessLogLevel
   , flags
   , run
   ) where
@@ -9,13 +8,11 @@ import Protolude
 
 import qualified Network.Wai as Wai
 import qualified Network.Wai.Handler.Warp as Warp
-import qualified Network.Wai.Middleware.RequestLogger as RL
 import qualified Options.Applicative as Options
 
 -- | Generic Warp configuration.
 data Config = Config
   { port :: Warp.Port -- ^ Port to listen on
-  , accessLogs :: AccessLogLevel -- ^ Level of access logs to display
   , debugExceptions :: Bool -- ^ Whether to show detailed exception information on 500s
   } deriving (Eq, Show)
 
@@ -24,42 +21,11 @@ flags :: Options.Parser Config
 flags =
   Config <$>
   Options.option Options.auto (fold [Options.long "port", Options.metavar "PORT", Options.help "Port to listen on"]) <*>
-  Options.option
-    (Options.eitherReader parseAccessLogs)
-    (fold [Options.long "access-logs", Options.help "How to log HTTP access", Options.value Disabled]) <*>
   Options.switch (fold [Options.long "debug-exceptions", Options.help "Show exceptions on 500."])
-  where
-
--- | What level of access logs to show.
-data AccessLogLevel
-  = Disabled -- ^ Don't show access logs.
-  | Enabled -- ^ Show Apache-style access logs.
-  | DevMode -- ^ Show detailed, colorful access logs. Not suitable in production.
-  deriving (Eq, Show)
-
--- | Construct an access log level from a command-line-friendly string.
---
--- Throws an error in the given monad if it can't recognize the level name.
-parseAccessLogs
-  :: (IsString error, IsString levelName, MonadError error m, Eq levelName)
-  => levelName -- ^ String-like thing describing the level. Either "none", "basic", or "dev".
-  -> m AccessLogLevel  -- ^ The corresponding access level.
-parseAccessLogs "none" = pure Disabled
-parseAccessLogs "basic" = pure Enabled
-parseAccessLogs "dev" = pure DevMode
-parseAccessLogs _ = throwError "One of 'none', 'basic', or 'dev'"
-
 
 -- | Run a web server for 'app'. Blocks until the server is shut down.
 run :: MonadIO io => Config -> Wai.Application -> io ()
-run config@Config {..} app = liftIO $ Warp.runSettings settings (logging app)
-  where
-    settings = warpSettings config
-    logging =
-      case accessLogs of
-        Disabled -> identity
-        Enabled -> RL.logStdout
-        DevMode -> RL.logStdoutDev
+run config@Config {..} app = liftIO $ Warp.runSettings (warpSettings config) app
 
 -- | Generate warp settings from config
 --
